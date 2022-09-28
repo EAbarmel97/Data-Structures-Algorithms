4b2edaca595d54b231f0b97923db4df7b37b9810
Enki Barra <enki.barmel97@ciencias.unam.mx>
2022-09-27 23:18:49 -0500

    cambios en SEC13

---
+++ CONTENIDOS /SEC13/pivot.py
@@ -0,0 +1,21 @@
+def swap(array, index1, index2):
+    #indercambiamos valor en index1 por valor en index2 y viceversa 
+    temp = array[index1] 
+    array[index1] = array[index2]
+    array[index2] = temp  
+
+def partition(array,pivot_index,end_index):
+    pivot = array[end_index] # 
+    index = pivot_index - 1 #var auxiliar 
+    for j in range(pivot_index, end_index):
+        if array[j] <= pivot: #comparacion con el pivote 
+            index += 1 #el indice se recorre a la derecha 
+            swap(array,index,j) #intercambiar las entradas de la lista
+            #index y j 
+    swap(array,index+1,end_index) #intercambiar las entradas 
+    return index + 1 #indice para la particion 
+
+my_list = [2,8,7,1,3,5,6,4]
+
+print(partition(my_list,0,6))
+print(my_list)
--- CONTENIDOS /SEC13/quick_sort.py
+++ CONTENIDOS /SEC13/quick_sort.py
@@ -1,2 +1,17 @@
-def quick_sort(array):
-    pass 
+from pivot import partition 
+
+def quick_sort(array,pivot_index,end_index):
+    #caso base 
+    if len(array) == 1:
+        return array 
+
+    ####### CASO RECURSIVO ########
+    if pivot_index < end_index:
+        p = partition(array, pivot_index, end_index)
+        quick_sort(array,pivot_index,p-1)
+        quick_sort(array,p+1,end_index)
+
+my_list = [4,6,1,7,3,2,5]
+print(my_list)
+quick_sort(my_list,0,6)
+print(my_list)               
